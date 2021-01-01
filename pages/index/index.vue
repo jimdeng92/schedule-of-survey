f67663cbc79532f166c04f27973a96e3ed0a08ac
jimdeng <jimdeng92@gmail.com>
2021-01-02 00:45:18 +0800

    调查表布局

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,13 @@
 <template>
-	<view>
-		index
+	<view class="index-container">
+		<u-grid :col="2">
+      <u-grid-item v-for="(item,index) in gridList" :key="item.id">
+        <u-icon :name="item.iconName" :size="46"></u-icon>
+        <view class="grid-text" @click="handleSkip(item.id)">{{item.gridText}}</view>
+      </u-grid-item>
+    </u-grid>
+    <view>TODO 公告...</view>
+    <u-toast ref="uToast" />
 	</view>
 </template>
 
@@ -8,12 +15,57 @@
 	export default {
 		data() {
 			return {
-				
+				gridList: [
+          {
+            id: 0,
+            iconName: 'file-text',
+            gridText: '调查表'
+          },
+          {
+            id: 1,
+            iconName: 'list',
+            gridText: '项目'
+          },
+          {
+            id: 2,
+            iconName: 'hourglass',
+            gridText: '沙漏'
+          }
+        ]
 			};
-		}
+		},
+    methods: {
+      handleSkip(id) {
+        let path = ''
+        switch (id){
+          case 0:
+            path = 'survey'
+            break
+          case 1:
+            path = 'project'
+            break
+          default:
+        }
+        if (!path) {
+          this.$refs.uToast.show({
+            title: '正在开发中...'
+          })
+          return
+        }
+        uni.navigateTo({
+          url: `/pages/${path}/${path}`
+        })
+      }
+    }
 	}
 </script>
 
 <style lang="scss">
-
+.index-container {
+  .grid-text {
+    font-size: 28rpx;
+    margin-top: 4rpx;
+    color: $u-type-info;
+  }
+}
 </style>
---
+++ pages/survey/survey.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="survey-list-container">
+    <view class="survey-list" v-for="item in 10" :key="item">
+      列表
+    </view>
+		<view class="survey-addition" @click="handleToSurveyAddition">
+      <u-icon name="edit-pen" :size="26" class="survey-addition-icon"></u-icon>
+      <text class="survey-addition-text">填表</text>
+    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+    methods: {
+      handleToSurveyAddition() {
+        uni.navigateTo({
+          url: '/pages/survey/survey-addition'
+        })
+      }
+    }
+	}
+</script>
+
+<style lang="scss">
+.survey-list-container {
+  .survey-addition {
+    color: red;
+    border-radius: 21px;
+    background-color: #fff;
+    height: 42px;
+    padding: 0 25px;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 2px 2px 5px $u-type-info;
+    .survey-addition-icon {
+      margin-right: 5px;
+    }
+  }
+}
+</style>
