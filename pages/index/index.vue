8cdda1bd899bc32a78ff7bbb66776998161eaea8
jimdeng <jimdeng92@gmail.com>
2021-01-03 19:51:07 +0800

    首页布局优化

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,14 @@
 <template>
 	<view class="index-container">
-		<u-grid :col="2">
-      <u-grid-item v-for="(item,index) in gridList" :key="item.id" @click="handleSkip(item.id)">
-        <u-icon :name="item.iconName" :size="46"></u-icon>
-        <view class="grid-text">{{item.gridText}}</view>
-      </u-grid-item>
-    </u-grid>
-    <view>TODO 公告...</view>
+		<u-row gutter="10">
+      <u-col span="6" v-for="item in gridList" :key="item.id" class="grid-col">
+        <view class="grid-item" @click="handleSkip(item.id)">
+          <u-icon :name="item.iconName" color="#fff" size="42"></u-icon>
+          <view class="grid-item-text">{{item.gridText}}</view>
+        </view>
+      </u-col>
+    </u-row>
+    <view>TODO 公告</view>
     <u-toast ref="uToast" />
 	</view>
 </template>
@@ -58,10 +60,29 @@
 <style lang="scss">
 .index-container {
   padding: 14px;
-  .grid-text {
-    font-size: 28rpx;
-    margin-top: 4rpx;
-    color: $u-type-info;
+  .grid-col {
+    .grid-item {
+      font-size: 28rpx;
+      color: $u-type-info;
+      margin-bottom: 5px;
+      text-align: center;
+      padding: 45px 0;
+      .grid-item-text {
+        font-size: 14px;
+        color: #fff;
+      }
+    }
+    &:nth-child(2n) .grid-item {
+      border-top-right-radius: 10px; 
+      border-bottom-right-radius: 10px; 
+      background: linear-gradient(135deg, #e66465, #9198e5);
+    }
+    &:nth-child(2n + 1) .grid-item {
+      border-top-left-radius: 10px; 
+      border-bottom-left-radius: 10px; 
+      background: linear-gradient(135deg, #3f87a6, #f69d3c);
+    }
   }
+  
 }
 </style>
